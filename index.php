28341acddd9abcb0b6d725853c87893898c9ff20
Sufyann34 <135757783+Sufyann34@users.noreply.github.com>
2023-08-27 23:17:02 -0700

    updated igs

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <?php include("includes/compatibility.php"); ?>
+      <meta name="description" content="">
+      <title>Home</title>
+      <?php include("includes/style.php"); ?>
+   </head>
+   <body>
+      <?php include("includes/header.php"); ?>
+      <section class="mainBanner" style="background-image:url(assets/images/banner/bg1.png); ">
+         <div class="container">
+         
+         </div>
+      </section>
+      <!-- game develop start -->
+      <section class="thirdban" style="background-image:url(assets/images/bg3.png); ">
+      <img class="anime1" src="assets/images/anime1.png" alt="">
+      <img class="anime2" src="assets/images/anime2.png" alt="">
+      <img class="anime3" src="assets/images/anime3.png" alt="">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12 text-center">
+               <div class="services">
+                  <h3>SERVICES</h3>
+                  <h1>GAME DEVELOPMENT SERVICES & ART SOLUTIONS</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sequi dicta quo. <br> Esse ipsa, tenetur accusantium nihil dolor delectus? Unde totam architecto omnis alias, modi veritatis laboriosam eaque eveniet dolor.</p>
+
+               </div>
+
+            </div>
+            <div class="col-md-12">
+               <div>
+                  <ul class="serve">
+                     <li class="first">
+                        <h1>Game Development</h1>
+                        <p>Our game development team uses the Unity platform to build a successful &  interactive 2D, 3D games with a flawless performance on every platform.</p>
+
+                     </li>
+                     <li class="last">
+                        <a href="">
+                           <img src="assets/images/plus.png" alt="">
+                        </a>
+
+                     </li>
+
+                  </ul>
+               </div>
+
+            </div>
+            <div class="col-md-12">
+               <div>
+                  <ul class="serve">
+                     <li class="first">
+                        <h1>2D Art</h1>
+                        <p>Our game development team uses the Unity platform to build a successful &  interactive 2D, 3D games with a flawless performance on every platform.</p>
+
+                     </li>
+                     <li class="last">
+                        <a href="">
+                           <img src="assets/images/plus.png" alt="">
+                        </a>
+
+                     </li>
+
+                  </ul>
+               </div>
+
+            </div>
+            <div class="col-md-12">
+               <div>
+                  <ul class="serve">
+                     <li class="first">
+                        <h1>3D Art</h1>
+                        <p>Our game development team uses the Unity platform to build a successful &  interactive 2D, 3D games with a flawless performance on every platform.</p>
+
+                     </li>
+                     <li class="last">
+                        <a href="">
+                           <img src="assets/images/plus.png" alt="">
+                        </a>
+
+                     </li>
+
+                  </ul>
+               </div>
+
+            </div>
+            <div class="col-md-12">
+               <div>
+                  <ul class="serve">
+                     <li class="first">
+                        <h1>Animation</h1>
+                        <p>Our game development team uses the Unity platform to build a successful &  interactive 2D, 3D games with a flawless performance on every platform.</p>
+
+                     </li>
+                     <li class="last">
+                        <a href="">
+                           <img src="assets/images/plus.png" alt="">
+                        </a>
+
+                     </li>
+
+                  </ul>
+               </div>
+
+            </div>
+            <div class="col-md-12">
+               <div>
+                  <ul class="serve">
+                     <li class="first">
+                        <h1>Game Design</h1>
+                        <p>Our game development team uses the Unity platform to build a successful &  interactive 2D, 3D games with a flawless performance on every platform.</p>
+
+                     </li>
+                     <li class="last">
+                        <a href="">
+                           <img src="assets/images/plus.png" alt="">
+                        </a>
+
+                     </li>
+
+                  </ul>
+               </div>
+
+            </div>
+
+         </div>
+
+      </div>
+         
+
+      </section>
+      <!-- game develop end -->
+
+      <!-- our partners start -->
+      <section class="fourban" style="background-image:url(assets/images/shape.png); ">
+         <div class="op text-center">
+            <h1>OUR PARTNERS</h1>
+
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-3">
+                  <div class="forbes">
+                     <h1>Forbes</h1>
+                     <h1>SmartCompany</h1>
+                  </div>                    
+               </div>
+               <div class="col-md-3">
+                  <div class="forbes">
+                     <h1>INC.</h1>
+                     <h1>NEWS</h1>
+                  </div>                 
+               </div>
+               <div class="col-md-3">
+                  <div class="forbes">
+                     <h1>Enterprenuer</h1>
+                     <h1>DAILY MAIL</h1> 
+                  </div>                
+               </div>
+               <div class="col-md-3">
+                  <div class="forbes">
+                     <h1>HUFFPOSTI</h1>
+                     <h1>Daily Telegraph</h1>
+                  </div>                 
+               </div>
+               
+
+            </div>
+
+         </div>
+
+      </section>
+      <!-- our partners end -->
+
+      <!-- our values start -->
+      <section class="fifthban" >
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 text-center">
+                  <div class="boost">
+                     <h1>OUR WORK</h1>
+                     <h2>Our VAlues Give Your Games <br> & Player The Ultimate Boost</h2>
+
+                  </div>                 
+               </div>
+               <div class="col-md-6">
+                  <div class="fire">
+                     <p>or 8+ years of experience in the industry of game art design & development, Innovative Game Studio Games managed to collect a solid game design portfolio of various artworks made in 2D or 3D & other styles & directions.</p>
+
+                     <img src="assets/images/ghoost.png" alt="">
+
+                  </div>
+
+               </div>
+               <div class="col-md-6">
+                  <div class="pictures">
+                     <img src="assets/images/lux.png" alt="">
+                     <img src="assets/images/sonu.png" alt="">
+                     <img src="assets/images/ghost.png" alt="">
+                     <img src="assets/images/darius.png" alt="">
+                  </div>   
+
+
+
+               </div>
+
+
+
+            </div>
+
+         </div>
+
+      </section>
+      
+      <!-- our values end -->
+      
+
+ 
+      <?php include("includes/footer.php"); ?>
+      <?php include("includes/scripts.php"); ?>
+   </body>
+</html>
